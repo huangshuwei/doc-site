5792361a0d28388e6e942bbbf90f6016afa8a6cb
huangshuwei <hsw@89520@163.com>
2018-06-20 21:05:10 +0800

    vue-cli-demo-site

---
+++ vue-cli-demo/precache-manifest.40db87a740645368835dc6e284d44fba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "78913d5af663e1644e1b",
+    "url": "/vue-cli-demo/js/chunk-vendors.78913d5a.js"
+  },
+  {
+    "revision": "529995709739c2609a3f",
+    "url": "/vue-cli-demo/js/app.52999570.js"
+  },
+  {
+    "revision": "a00978a0f19c09c05a3eca9c167f0c8d",
+    "url": "/vue-cli-demo/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/vue-cli-demo/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "529995709739c2609a3f",
+    "url": "/vue-cli-demo/css/app.90268f40.css"
+  }
+];
