0aa3fed5d7a2e2591a366834dcb3783b026d52cb
huangshuwei <hsw89520@163.com>
2020-05-26 14:58:03 +0800

    add theme-switcher-cli demo

---
+++ theme-switcher-cli-demo/precache-manifest.b76f27e7867e2539f8c37f6b5752a1a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ccee384c130acd025f6ce18709f66e4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c195bb08982c0a012550",
+    "url": "./static/js/2.fab3eb8f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.fab3eb8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "896d7b35353059617c73",
+    "url": "./static/js/main.feef61a9.chunk.js"
+  },
+  {
+    "revision": "c9099bf76ee11446ef6a",
+    "url": "./static/js/runtime-main.5d41aa64.js"
+  }
+]);
--- theme-switcher-cli-demo/service-worker.js
+++ theme-switcher-cli-demo/service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.3.0/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/vue-cli-demo/precache-manifest.40db87a740645368835dc6e284d44fba.js"
+  "./precache-manifest.b76f27e7867e2539f8c37f6b5752a1a2.js"
 );
 
-workbox.core.setCacheNameDetails({prefix: "vue-cli-demo"});
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,5 +31,9 @@ workbox.core.setCacheNameDetails({prefix: "vue-cli-demo"});
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
