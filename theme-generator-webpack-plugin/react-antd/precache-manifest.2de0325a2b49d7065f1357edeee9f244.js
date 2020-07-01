1d52ac72d80ec586377d575184ddb943937e56d9
huangshuwei <hsw89520@163.com>
2020-07-01 16:45:21 +0800

    add demo

---
+++ theme-generator-webpack-plugin/react-antd/precache-manifest.2de0325a2b49d7065f1357edeee9f244.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a350464838a573e5a37647ba111b7496",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a9d0183abfa67d9179b1",
+    "url": "./static/js/2.51a6b9d2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.51a6b9d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f4b7b761ab27b5460bf",
+    "url": "./static/js/main.4feda197.chunk.js"
+  },
+  {
+    "revision": "c9099bf76ee11446ef6a",
+    "url": "./static/js/runtime-main.5d41aa64.js"
+  },
+  {
+    "revision": "13adba642a0a7a3abc23c71b57b8fc38",
+    "url": "./theme-black.css"
+  },
+  {
+    "revision": "44d1c4b4773a2ea21d709195107d72bc",
+    "url": "./theme-blue.css"
+  },
+  {
+    "revision": "f435e3b6d7512af78e269324a713a2d9",
+    "url": "./theme-orange.css"
+  },
+  {
+    "revision": "bf977fdc3e89a0fb61101793a334374e",
+    "url": "./theme-red.css"
+  }
+]);
